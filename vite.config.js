84c1bfc6cabdc954be8276e9dd7a58670d762b88
Paweł Bród <pawel.brod@gmail.com>
2023-12-14 21:31:42 +0100

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,6 @@ export default {
     {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+        sourcemap: false // Add sourcemap
     },
 }
